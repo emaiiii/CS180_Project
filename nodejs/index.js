ed7981a0e3178bd179d8a2eb07d7696ab25add39
jxu103 <43682343+jxu103@users.noreply.github.com>
2020-04-17 12:47:14 -0700

    added database and server update

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,15 +1,42 @@
 const http = require('http');
+const url = require('url');
+const fs = require('fs');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
+/*
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World\n');
 
-  console.log('recevied request from client');
+  console.log('recevied request from client' +req.url);
 
+});
+*/
+
+const server = http.createServer(function (req,res) {
+  if(req.url === '/') {
+    res.writeHead(200, {'Content-type' : 'text/plain'});
+    res.end('Hello World\n');
+  }
+  
+  else if(req.url != '/favicon.ico') {
+    var q = url.parse(req.url,true);
+
+    var qdata = q.query;
+    console.log(qdata);
+
+    if(qdata.team != 0) {
+      res.writeHead(200, {'Content-type': 'text/plain'});
+      res.write(qdata.team);
+      res.end();
+    }
+
+    console.log('recieved request');
+  }
+  
 });
 
 server.listen(port, hostname, () => {
