bf92427a200327ab88da5bd63a8bf6a86c9e3b04
AnshNagpal <43831280+AnshNagpal@users.noreply.github.com>
2020-05-11 07:39:17 -0700

    Update AnalyzeFragment.java

--- BestNBAApp/BestNBAApp/app/src/main/java/com/mai/airwi/bestnbaapp/AnalyzeFragment.java
+++ BestNBAApp/BestNBAApp/app/src/main/java/com/mai/airwi/bestnbaapp/AnalyzeFragment.java
@@ -48,16 +48,73 @@ public class AnalyzeFragment extends Fragment {
     Button prevButton;
     Button refreshButton;
 
+    int pos = 0; // POSITION of stat view in set
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_analyze, container, false);
 
-    statDisplay = (TextView)view.findViewById((R.id.statDisplay));
+        statDisplay = (TextView)view.findViewById((R.id.statDisplay));
         nextButton = (Button)view.findViewById(R.id.nextButton);
         prevButton = (Button) view.findViewById(R.id.prevButton);
         refreshButton = (Button) view.findViewById(R.id.refresh);
 
+        // make init request: userset is at 0 or at null
+        // if at any point null, redirect text to say no items in set
+        // if 0, load data for 0 averages
+        // next sends up (if at max send back to 0) --> CIRCULAR
+        // prev is like next in reverse --> CIRCULAR
+
+        String playerName = ""; // FIXME: fxn to get player name @ pos
+        final String statURL = server_url + "?playername=" + playerName + "&&username=" + username;
+
+        // HTTP request
+        //
+        final RequestQueue requestQueue = Volley.newRequestQueue( getActivity() );
+
+        Log.i("ULR", statURL);
+        StringRequest statRequest = new StringRequest(Request.Method.POST, statURL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.i("Info", "Request complete.");
+
+                        if (response.equals("no player found")) {
+                            statDisplay.setText("no player found");
+                        }
+                        else {
+                            List<String> list = new ArrayList<String>();
+                            String toDisplay;
+
+                            list = read(response);
+
+                            // code for team display
+                            Team teamData = new Team(list);
+
+                            // FIXME: parse details to display
+                            toDisplay = "Team ID: " + teamData.getTeamID();
+                            toDisplay = toDisplay + "\nMin. Year: " + teamData.getMinYear();
+                            toDisplay = toDisplay + "\nMax. Year: " + teamData.getMaxYear();
+
+                            statDisplay.setText(toDisplay);
+
+                        }
+
+                        requestQueue.stop();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        error.printStackTrace();
+                        requestQueue.stop();
+                    }
+                }
+        );
+        //
+
+        requestQueue.add(statRequest);
 
         // FIXME: load data from intent
         //response = getIntent().getExtras().getString("response");
@@ -67,6 +124,10 @@ public class AnalyzeFragment extends Fragment {
         list = new ArrayList<String>();
         list = read(response);
         Player player = new Player(list);
+
+
+
+
 */
 
         return view;
