1228dde31cddea65c629b46239c4bc41501d4702
emaiiii <airwickmai@gmail.com>
2020-05-16 13:53:04 -0700

    analyses results pages

--- BestNBAApp/BestNBAApp/app/src/main/java/com/mai/airwi/bestnbaapp/playerAnalyses.java
+++ BestNBAApp/BestNBAApp/app/src/main/java/com/mai/airwi/bestnbaapp/playerAnalyses.java
@@ -1,13 +1,32 @@
 package com.mai.airwi.bestnbaapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class playerAnalyses extends AppCompatActivity {
+
+    Button getAvgButton;
+    Button getRatingButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player_analyses);
+
+        getAvgButton = (Button)findViewById(R.id.getAvgButton);
+        getRatingButton = (Button)findViewById(R.id.getRatingButton);
+
+        getAvgButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Log.i("Info", "Get averages button clicked");
+
+
+            }
+        });
     }
 }
